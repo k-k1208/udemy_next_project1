79af3b08968ccbaa5a64538e5322a3e5637ccc90
xxxx <xxxxxx@gmail.com>
2022-02-04 17:57:24 +0900

    blogページの概要完成

---
+++ pages/blog-page.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Layout from '../components/Layout'
+
+const Blog: NextPage = () => {
+  return (
+    <>
+      <Layout title="Blog">
+        <p className="text-4xl">Blog</p>
+      </Layout>
+    </>
+  )
+}
+
+export default Blog
---
+++ pages/contact-page.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Layout from '../components/Layout'
+
+const Contact: NextPage = () => {
+  return (
+    <>
+      <Layout title="Contact">
+        <p className="text-4xl">Contact</p>
+      </Layout>
+    </>
+  )
+}
+
+export default  Contact
